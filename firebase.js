d196bd068da0acfb9c7597562c9e4c871e33b1c2
priyanka4546 <hcpriyanka12@gmail.com>
2025-05-09 22:15:19 +0530

    modified firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDEINMQl7d3Pq3bWUDErTMC5Z-P3QQ9q4Q",
-  authDomain: "foodforecast-63003.firebaseapp.com",
-  projectId: "foodforecast-63003",
-  storageBucket: "foodforecast-63003.firebasestorage.app",
-  messagingSenderId: "659015203148",
-  appId: "1:659015203148:web:3430143578cf7a927d37e9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
